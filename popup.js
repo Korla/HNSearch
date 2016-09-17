7c7ebb0701dd1e5ced8fb4cb117af6dfca39e394
korla <johannes.bengtsson@raysearchlabs.com>
2016-09-17 23:23:53 +0200

    Cleanup

--- popup.js
+++ popup.js
@@ -5,11 +5,12 @@ let serialize = obj => '?'+Object.keys(obj).reduce((a,k)=>{a.push(k+'='+encodeUR
 let ajaxGet = url => fetch(url).then(d => d.json());
 let toDateString = date => (new Date(date)).toUTCString();
 
-let createNode = (tag, children, text, onclick) => {
+let createNode = (tag, children, onclick) => {
   var node = document.createElement(tag);
-  children.forEach(c => node.appendChild(c));
-  if(text) {
-    node.appendChild(document.createTextNode(text));
+  if(Array.isArray(children)) {
+    children.forEach(c => node.appendChild(c));
+  } else {
+    node.appendChild(document.createTextNode(children));
   }
   node.onclick = onclick;
   return node;
@@ -17,18 +18,16 @@ let createNode = (tag, children, text, onclick) => {
 
 let createRow = data => createNode('tr', [
   createLinkButtonTd(data.url, data.title),
-  createTd(data.author),
-  createTd(data.date),
-  createTd(data.nbrOfComments)
+  createNode('td', data.author),
+  createNode('td', data.date),
+  createNode('td', data.nbrOfComments)
 ]);
 
 let createLinkButtonTd = (url, title) => {
-  let node = createNode('td', [], title, () => window.open(url));
+  let node = createNode('td', title, () => window.open(url));
   node.setAttribute('title', title);
   return node;
 }
-
-let createTd = text => createNode('td', [], text);
 
 let getCurrentTabUrl = () =>
   new Promise((resolve, reject) =>
@@ -36,7 +35,6 @@ let getCurrentTabUrl = () =>
 
 let hn = {
   name: 'HackerNews',
-  cssClass: 'HackerNews',
   search: query =>
     ajaxGet('https://hn.algolia.com/api/v1/search' + serialize({ hitsPerPage: 5, restrictSearchableAttributes: 'url', query }))
       .then(({hits}) => hits.filter(hit => hit.title).map(hn.toRowData_)),
@@ -50,8 +48,7 @@ let hn = {
 }
 
 let reddit = {
-  name: '/r/Programming',
-  cssClass: 'rProgramming',
+  name: 'Reddit',
   search: q => ajaxGet(`https://www.reddit.com/r/programming/search.json` + serialize({sort: 'comment', q}))
     .then(d => d && d.data ? d.data.children : [])
     .then(hits => hits.map(d => d.data).map(reddit.toRowData_)),
@@ -67,22 +64,23 @@ let reddit = {
 let searches = [hn, reddit];
 let table = document.querySelector('table tbody');
 
-let processResults = ({name, cssClass}, rows) => {
-  if(!rows.length){
+let processResults = ({name}, rows) => {
+  if(!rows.length) {
     let noHits = createNode('tr', [
-      createNode('td', [], 'No hits on ' + name),
-      createNode('td', [], '-'),
-      createNode('td', [], '-'),
-      createNode('td', [], '-')
+      createNode('td', 'No hits on ' + name),
+      createNode('td', '-'),
+      createNode('td', '-'),
+      createNode('td', '-')
     ]);
-    noHits.classList.add(cssClass);
+    noHits.classList.add(name);
     table.appendChild(noHits);
   } else{
     rows
       .sort((a, b) => a.nbrOfComments > b.nbrOfComments ? -1 : 1)
       .filter((row, i) => i < 3)
-      .map(createRow).forEach(c => {
-        c.classList.add(cssClass);
+      .map(createRow)
+      .forEach(c => {
+        c.classList.add(name);
         table.appendChild(c);
       });
   }
